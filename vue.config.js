10c8e9af72be03037603ca1fc2b27bbef8c35b70
springleo <782655835@qq.com>
2019-01-10 19:25:11 +0800

    add global sass && analysis report

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // 是否为生产环境构建生成 source map
+    productionSourceMap: false,
+
+    // CSS 相关选项
+    css: {
+        loaderOptions: {
+            sass: {
+                data: `@import "@/assets/css/variables.scss";`
+            }
+        }
+    },
+
+    chainWebpack: config => {
+        if (process.env.npm_config_report) {
+            config.plugin('webpack-bundle-analyzer').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+        }
+    }
+}
